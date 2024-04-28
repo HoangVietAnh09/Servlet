736ae82aba1d71d1404a48b74b92f848816e3504
HoangVietAnh09 <vietanh092004@gmail.com>
2024-04-28 11:08:52 +0700

    Servlet

---
+++ src/main/java/com/Controller/login.java
@@ -0,0 +1,30 @@
+package com.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "loginServlet", urlPatterns = "/login")
+public class login extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        if(username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("admin")){
+            resp.sendRedirect("/hello");
+        }else{
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+    }
+}
