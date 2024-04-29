2587661b7b68614c45d408f50ed5aa458f798f80
HoangVietAnh09 <vietanh092004@gmail.com>
2024-04-29 18:20:53 +0700

    info in table

---
+++ src/main/java/com/model/uses.java
@@ -0,0 +1,59 @@
+package com.model;
+
+public class uses {
+    private int id;
+    private int age;
+    private String fullname;
+    private String email;
+    private String password;
+    private String dateTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPasswd(String password) {
+        this.password = password;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+}
+
