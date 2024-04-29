00de50e850af5404893f65514ebc3804ad606d8d
HoangVietAnh09 <vietanh092004@gmail.com>
2024-04-29 18:13:53 +0700

    registForm

---
+++ src/main/java/com/Controller/regist.java
@@ -0,0 +1,18 @@
+package com.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "regist", urlPatterns = "/regist")
+public class regist extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+    }
+}
